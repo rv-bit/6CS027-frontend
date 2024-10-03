9ffa24ef3c2f493986927176ac292a30ed4bffc2
Robert Vilcu <robertvilcu89@gmail.com>
2024-10-03 22:50:16 +0100

    init: front end of project

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <View className="flex-1 flex-col gap-5 items-center justify-center bg-gray-500">
+            <Text>Open up App.js to start working on your app! yesyeseyjhjhjhjs</Text>
+            <StatusBar style="auto" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+            <Pressable
+                className="bg-gray-200 p-5 rounded-lg uppercase text-white"
+                onPress={() => alert('Hello, world!')}
+            >
+                <Text>Press me</Text>
+            </Pressable>
+        </View>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+module.exports = {
+    content: [
+        "./App.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
