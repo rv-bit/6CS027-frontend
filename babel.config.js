4110ab776723d644c854d81d607281865cda49e2
Robert Vilcu <robertvilcu89@gmail.com>
2024-10-04 10:39:08 +0100

    update: dependencies + add new router

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+const plugin = require("tailwindcss");
+
 module.exports = function(api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        ["nativewind/babel", { tailwindConfig: "./tailwind.config.js" }],
+        plugins: [
+            "nativewind/babel",
+        ]
     };
 };
